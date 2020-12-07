1c2d0ccfe062cf042e40d085e1f67669a7056aa3
Prateek Varma <prtk.varma@gmail.com>
2020-12-07 09:48:56 -0600

    working on users schema

---
+++ app/users/schema.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+#Custom type class, used to define users. Inherits from Django's built in User Model
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+#create mutation to create user
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    class mutate(self, info, username, password, email):
+        #Use the default Django's user model
+        user = get_user_model()(
+            username = username
+            email = email
+        )
+        #password needs to be set with set_password function
+        user.set_password(password)
+        user.save()
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
