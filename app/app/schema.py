1312004a313fb1df49b9a428cc9eb7310a8bb514
Prateek Varma <prtk.varma@gmail.com>
2020-12-08 13:12:35 -0600

    setup JWT, and added the 'me' query to schema

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,13 +1,16 @@
 import graphene
 import tracks.schema
 import users.schema
+import graphql_jwt
 
 #main Query class, that will take query classes from all other django apps
 class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation ,tracks.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 #schema is defined here in main schema, instead of every app's schema
 schema = graphene.Schema(query=Query, mutation=Mutation)
